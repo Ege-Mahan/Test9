430ca5a3da5627e7fbda57fce9cbc7670bbc7094
Ege-Mahan <142576060+Ege-Mahan@users.noreply.github.com>
2023-08-18 16:39:54 +0300

    Add files via upload

--- test9.js
+++ test9.js
@@ -3,7 +3,15 @@
 
 const pictures = ["1.jpg", "2.jpg", "3.jpeg", "4.webp", "5.avif"];
 
-let curretPicture = 1;
+const pic1 = "White Kitten";
+const pic3 = "Vicious Cat";
+const pic4 = "Shark";
+const pic5 = "Darkness";
+const pic2 = "Orange";
+
+const comments = [pic1, pic2, pic3, pic4, pic5];
+
+let currentPicture = 1;
 
 document.getElementById("backward").addEventListener("click", (e)=>{
 
@@ -19,29 +27,70 @@ document.getElementById("forward").addEventListener("click", (e)=>{
 
 function forwards() {
 
-    ++curretPicture;
+    ++currentPicture;
 
-    if(curretPicture == pictures.length) {
+    if(currentPicture == pictures.length) {
 
-        curretPicture = 0;
+        currentPicture = 0;
 
     }
 
-    document.getElementById("picture").src = pictures[curretPicture];
+    document.getElementById("picture").classList.remove("pictureAnimation1");
+    document.getElementById("picture").classList.remove("pictureAnimation2");
+    document.getElementById("comment").classList.remove("textAnimation1");
+    document.getElementById("comment").classList.remove("textAnimation2");
+    document.getElementById("comment").classList.remove("textAnimation3");
+    document.getElementById("comment").classList.remove("textAnimation4");
+
+    document.getElementById("picture").addEventListener("animationend", function() {
+		
+        document.getElementById("picture").classList.remove("pictureAnimation1");
+        document.getElementById("comment").classList.remove("textAnimation2");
+    
+        document.getElementById("picture").src = pictures[currentPicture];
+        document.getElementById("comment").innerHTML = comments[currentPicture];
+    
+        document.getElementById("picture").classList.add("pictureAnimation2");
+        document.getElementById("comment").classList.add("textAnimation1");
+    
+    });
+
+    document.getElementById("picture").classList.add("pictureAnimation1");
+    document.getElementById("comment").classList.add("textAnimation2");
     
 }
 
 function backwards() {
 
-    --curretPicture;
+    --currentPicture;
 
-    if(curretPicture == -1) {
+    if(currentPicture == -1) {
 
-        curretPicture = pictures.length-1;
+        currentPicture = pictures.length-1;
 
     }
 
-    document.getElementById("picture").src = pictures[curretPicture];
-    
-}
+    document.getElementById("picture").classList.remove("pictureAnimation1");
+    document.getElementById("picture").classList.remove("pictureAnimation2");
+    document.getElementById("comment").classList.remove("textAnimation1");
+    document.getElementById("comment").classList.remove("textAnimation2");
+    document.getElementById("comment").classList.remove("textAnimation3");
+    document.getElementById("comment").classList.remove("textAnimation4");
 
+    document.getElementById("picture").addEventListener("animationend", function() {
+		
+        document.getElementById("picture").classList.remove("pictureAnimation1");
+        document.getElementById("comment").classList.remove("textAnimation4");
+    
+        document.getElementById("picture").src = pictures[currentPicture];
+        document.getElementById("comment").innerHTML = comments[currentPicture];
+    
+        document.getElementById("picture").classList.add("pictureAnimation2");
+        document.getElementById("comment").classList.add("textAnimation3");
+    
+    });
+
+    document.getElementById("picture").classList.add("pictureAnimation1");
+    document.getElementById("comment").classList.add("textAnimation4");
+
+}
